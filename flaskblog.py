3ef0efcaa30ed932696611c7c1b4e2556f0090e1
barnez299 <barnez29@gmail.com>
2021-01-04 23:36:30 +0200

    forms validations and errors working

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_wtf.csrf import CSRFProtect
 from forms import RegistrationForm, LoginForm
 
@@ -43,12 +43,21 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
