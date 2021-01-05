373c2225550edd8b140315aef4b1c656fe578a05
barnez299 <barnez29@gmail.com>
2021-01-06 01:08:16 +0200

    authentication and login and logout working

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,16 +2,24 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost/blogposts'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
 
 SECRET_KEY = os.urandom(32)
 
 app.config['SECRET_KEY'] = SECRET_KEY
 csrf = CSRFProtect(app)
 
-from flaskblog import routes
+# from flaskblog import routes
