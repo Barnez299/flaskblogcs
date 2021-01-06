2488edb2a47eb1b0410c318dedf66e22b83c2745
barnez299 <barnez29@gmail.com>
2021-01-06 23:16:57 +0200

    get single post route created

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -114,3 +114,10 @@ def new_post():
         flash('Your post has been created', category='success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
+
+# Step 2 - View single post (get_one) - user need not to be authenticated or logged in
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
