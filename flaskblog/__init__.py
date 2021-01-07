655dd4710b189634f91155526be6e125abbb6766
barnez299 <barnez29@gmail.com>
2021-01-07 23:25:37 +0200

    reset password routes added token issue to be resolved

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 
 app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost/blogposts'
 db = SQLAlchemy(app)
@@ -15,6 +17,19 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'barnaby.davids@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'Paxxword9###'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = '***********'
+app.config['MAIL_PASSWORD'] = '*******'
+
+mail = Mail(app)
 
 
 
